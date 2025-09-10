78e1ce984303111a55609a6587e68ef078650dc1
Xa C24 <xr.piallu@gmail.com>
2025-09-10 19:33:04 +0200

    Add langue fr en et cn pour Nos engagements

--- src/components/Accueil.vue
+++ src/components/Accueil.vue
@@ -238,7 +238,9 @@
 </div>
 
 <br><br>
-<h2 class="page-blanche_app-title">{{ $t('Nos engagements pour un impact positif') }}</h2>
+<h2 class="page-blanche_app-title">
+   {{ $t('Nos_engagements.title') }}
+</h2>
 
 <br><br>
 <br><br>
@@ -276,13 +278,13 @@
       v-if="hoveredLogo === 0"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[380px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Pas de Pauvreté</h3>
-      <p>
-        Nous prévoyons d’embaucher de la main d’œuvre locale quel que soit la zone
-        d’implantation, y compris dans les zones à faible développement économique.
-        Nous investissons dans les entreprises locales, encourageons les start-ups
-        et posons la première pierre d’une lutte efficace contre la pauvreté.
+      <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.Pas_de_Pauvrete_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.Pas_de_Pauvrete_texte') }}
       </p>
+
     </div>
 
      <!-- Bulle d'info (uniquement pour le logo 2 / index 1) -->
@@ -290,15 +292,13 @@
       v-if="hoveredLogo === 1"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[350px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Faim "zéro"</h3>
-      <p>
-        Nous permettons l’avènement d’une agriculture durable sans surexploitation des sols
-        et en permettant aux agriculteurs locaux d’exploiter une partie de nos ressources
-        sans dépense colossale en matière de coût de transport.<br><br>
-        Nous proposons également à ces derniers un complément de revenu
-        et du digestat conforme aux critères de l’agriculture biologique afin de maximiser leurs rendements.<br><br>
-        Nous permettons ainsi aux exploitants de produire sur place des cultures alimentaires.
+    <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.Faim_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.Faim_texte') }}
       </p>
+    
     </div>
 
     <!-- Bulle d'info (uniquement pour le logo 3 / index 2) -->
@@ -306,13 +306,15 @@
       v-if="hoveredLogo === 2"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[350px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Bonne santé et bien-être</h3>
-      <p>
-       De part notre activité de valorisation de déchets et de production de plantes a visées thérapeutique, 
-       nous permettons le développement et la synthèses de traitement contre les addictions, la mortalité causée 
-       par certaines pathologies et les troubles mentaux et psychiques. Nous permettons également de réduire drastiquement 
-       les affections causées par les polluants, les produits chimiques et les amas de déchets.
+
+    <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.BonneSante_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.BonneSante_texte') }}
       </p>
+      
+    
     </div>
 
     <!-- Bulle d'info (uniquement pour le logo 4 / index 3) -->
@@ -320,14 +322,14 @@
       v-if="hoveredLogo === 3"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[350px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Accès à une éducation de qualité</h3>
-      <p>
-       Nous sommes susceptible de faire appel à toute une variété de compétences et embaucherons un personnel disposant 
-       de tous types de qualifications. Nous souhaitons avoir, en interne, la quasi-totalité du spectre éducatif. 
-       Nous formerons et financeront également les études et la formation de jeunes prometteurs. 
-       Nous investirons également dans des campagnes d’éducation auprès des populations locales 
-       (notamment dans le cadre de l’apprentissage au tri et à la mise à la poubelle)
+
+    <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.education_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.edcucation_texte') }}
       </p>
+      
     </div>
 
     <!-- Bulle d'info (uniquement pour le logo 5 / index 4) -->
@@ -335,13 +337,14 @@
       v-if="hoveredLogo === 4"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[350px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Égalité entre les sexes</h3>
-      <p>
-      Nous avons pour objectif d’atteindre une parité parfaite entre les sexes. Les salaires seront attribués en fonction des compétences,
-      du résultat et du travail fourni. Aucune discrimination salariale n’est mise en place en fonction du sexe et l’entreprise assure 
-      à tous les mêmes opportunités de carrière et d’avancement. Nous serons également très vigilant concernant nos lieux d’implantations 
-      et nos partenaires et veilleront à ne pas faciliter les structures ou les états n’allant pas dans ce sens.
+    <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.egalite_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.egalite_texte') }}
       </p>
+      
+      
     </div>
 
     <!-- Bulle d'info (uniquement pour le logo 6 / index 5) -->
@@ -349,14 +352,13 @@
       v-if="hoveredLogo === 5"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[350px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Accès à l'eau salubre et à l'assainissement</h3>
-      <p>
-      Nous proposons une solutions permettant de purifier l’eau, de la rendre potable ou exploitable dans l’agriculture. 
-      Ce faisant, nous permettons la ré-exploitation de cette ressource vitale. En d’autres termes, nous exploitons, valorisons 
-      et purifions les eaux usées et contaminées. En évitant le rejet en pleine nature d’eau souillé et polluée, nous préservons 
-      ainsi les courts d’eau et leurs écosystèmes. 
-      Ce faisant, nous permettons également un accès facilité à une eau potable pour tous.
+    <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.eauSalubre_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.eauSalubre_texte') }}
       </p>
+      
     </div>
 
      <!-- Bulle d'info (uniquement pour le logo 7 / index 6) -->
@@ -364,12 +366,14 @@
       v-if="hoveredLogo === 6"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[350px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Énergies fiables, durables et modernes, à un coût abordable</h3>
-      <p>
-     Notre énergie est 100% renouvelable, nous exploitons même la chaleur pour chauffer ou recréer de l’électricité. 
-     De part notre volonté d’expansion a l’international et des caractéristiques de notre modèle, nous avons la possibilité 
-     de développer l’exploitation des énergies et des réseaux de chaleur renouvelables partout sur le globe.
+
+    <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.energieFiable_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.energieFiable_texte') }}
       </p>
+      
     </div>
 
      <!-- Bulle d'info (uniquement pour le logo 8 / index 7) -->
@@ -377,12 +381,14 @@
       v-if="hoveredLogo === 7"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[350px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Accès à des emplois décents</h3>
-      <p>
-     De part le caractère innovant du projet, la possibilité pour ce dernier de permettre la réhabilitations de zones polluées
-      et pouvant être exploitée dans le cadre d’activités touristiques, le fait que notre équipe soit principalement composée de 
-      jeunes nous apparaît être un argument supplémentaire.
+
+    <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.emploi_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.emploi_texte') }}
       </p>
+      
     </div>
 
     <!-- Bulle d'info (uniquement pour le logo 9 / index 8) -->
@@ -390,12 +396,14 @@
       v-if="hoveredLogo === 8"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[350px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Industrie, innovation et infrastructure</h3>
-      <p>
-      L’économie circulaire et l’autonomie en terme d’énergie et de ressources sont la base même de notre modèle.
-       Avec cette autonomie énergétique, vient irrémédiablement la possibilité de développer tout un écosystème économique 
-       et industriel basé autour des énergies renouvelables.
+
+     <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.industrie_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.industrie_texte') }}
       </p>
+      
     </div>
 
     <!-- Bulle d'info (uniquement pour le logo 10 / index 9) -->
@@ -403,13 +411,14 @@
       v-if="hoveredLogo === 9"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[350px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Réduction des inégalités</h3>
-      <p>
-      Notre équipe est comprend autant de femmes que d’hommes à des postes clés et est également composés d’individus de plusieurs nationalités. 
-      En tant que multinationale en devenir, nous avons à cœur de faire appels aux talents partout où ils sont. 
-      Chacun de nos employés bénéficieront des mêmes opportunités et accèderont à des postes clés en fonction de leurs mérites.
-      Le PDG de l’entreprise est lui-même titulaire d’une RQTH et est donc reconnu travailleur handicapé.
+
+     <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.reduction_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.reduction_texte') }}
       </p>
+     
     </div>
 
     <!-- Bulle d'info (uniquement pour le logo 11 / index 10) -->
@@ -417,13 +426,14 @@
       v-if="hoveredLogo === 10"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[350px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Villes et communautés durables</h3>
-      <p>
-      Une ville propre et bénéficiant d’une électivité verte c’est une ville durable. Nous avons à cœur d’assurer l’autonomie
-       énergétique de ces ensembles tout en faisant la promotion des mobilités vertes et bas-carbone. Par l’organisation d’un réseau 
-       de chaleur et de froid issu de procédés renouvelables, nous participons également à la réduction des dépenses et à la baisse 
-       de la consommation de gaz et d’électricité.
+
+     <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.villes_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.villes_texte') }}
       </p>
+    
     </div>
 
      <!-- Bulle d'info (uniquement pour le logo 12 / index 11) -->
@@ -431,13 +441,14 @@
       v-if="hoveredLogo === 11"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[350px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Consommation et production responsables</h3>
-      <p>
-      C’est le cœur de notre modèle : nous récupérons toute presque matière difficilement exploitable et presque tous déchets
-       et nous les transformons en ressources. Nous encouragerons et accompagnement également les états dans leur politique de passage
-      vers une économie circulaire et l’organisation responsable du traitement de leurs déchets. 
-      Nous permettons également aux entreprise un service de récupération de leurs déchets afin d’éviter leur déversement dans la nature.
+
+    <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.consommation_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.consommation_texte') }}
       </p>
+      
     </div>
 
      <!-- Bulle d'info (uniquement pour le logo 13 / index 12) -->
@@ -445,13 +456,14 @@
       v-if="hoveredLogo === 12"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[350px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Lutte contre les changements climatiques</h3>
-      <p>
-      Nous réduisons les émissions de gaz à effet de serre issue de la décomposition de certaines matières, 
-      nous ne rejetons pas de GES et nous permettons la réduction voire la disparition totale de la consommation d’énergie fossile 
-      fortement émettrices de GES comme le charbon. Par la proximité et l’interconnexion de nos installations entre-elles étaient les 
-      zones de concentration de déchets, nous réduisons également les besoins de transport en valorisant et produisant sur place.
+
+    <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.lutteChangement_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.lutteChangement_texte') }}
       </p>
+     
     </div>
 
     <!-- Bulle d'info (uniquement pour le logo 14 / index 13) -->
@@ -459,14 +471,14 @@
       v-if="hoveredLogo === 13"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[350px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Conserver et exploiter de manière durable les océans et les mers aux fins du développement durable
-</h3>
-      <p>
-      Nous réduisons les émissions de gaz à effet de serre issue de la décomposition de certaines matières, 
-      nous ne rejetons pas de GES et nous permettons la réduction voire la disparition totale de la consommation d’énergie fossile 
-      fortement émettrices de GES comme le charbon. Par la proximité et l’interconnexion de nos installations entre-elles étaient les 
-      zones de concentration de déchets, nous réduisons également les besoins de transport en valorisant et produisant sur place.
+
+    <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.conserver_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.conserver_texte') }}
       </p>
+      
     </div>
 
     <!-- Bulle d'info (uniquement pour le logo 15 / index 14) -->
@@ -474,14 +486,14 @@
       v-if="hoveredLogo === 14"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[350px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Vie terrestre
-</h3>
-      <p>
-      Notre modèle permet la conservation et la restauration de la vie dans le sol. En évitant son appauvrissement et en l’enrichissant, 
-      nous limitons drastiquement les besoins en engrais chimiques et l’épuisement des sols. En facilitant l’agriculture durable et 
-      l’exploitation de ressources renouvelables, nous permettons également la protection de la biodiversité et assurons 
-      une exploitation raisonnée des ressources.
+
+    <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.vieTerrestre_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.vieTerrestre_texte') }}
       </p>
+    
     </div>
 
     <!-- Bulle d'info (uniquement pour le logo 16 / index 15) -->
@@ -489,12 +501,14 @@
       v-if="hoveredLogo === 15"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[350px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Justice et paix
-</h3>
-      <p>
-      En évitant les famines, en assurant à tous un accès à l’eau et en réduisant les inégalités économiques au sein des populations locales : 
-      nous limitons les tensions et les risques de guerre.
+
+     <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.justicePaix_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.justicePaix_texte') }}
       </p>
+
     </div>
 
      <!-- Bulle d'info (uniquement pour le logo 17 / index 16) -->
@@ -502,13 +516,12 @@
       v-if="hoveredLogo === 16"
       class="absolute top-1/2 left-1/2 w-[280px] sm:w-[350px] bg-white p-4 rounded-xl shadow-xl text-sm sm:text-base text-gray-800 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center"
     >
-      <h3 class="font-bold text-lg mb-2" style="color: #8BC34A;">Partenariats pour la réalisation des objectifs
-</h3>
-      <p>
-      Notre démarche est basé sur le partenariat et nécessite l’action conjointe de plusieurs partenaires privés et d’institutions publiques.
-       Nous avons communiqué, informé et décrit notre modèle avec précision et gratuitement et avons également fourni un service 
-       de conseil gratuit aux collectivités. Nous prévoyons également des investissements à l’internationale 
-       (y compris dans les pays en développement).
+
+    <h3 class="font-serif font-bold text-xl mb-2" style="color: #8BC34A;">
+        {{ $t('Nos_engagements.Partenariats_title') }}</h3>
+
+      <p class="font-serif text-lg leading-relaxed">
+      {{ $t('Nos_engagements.Partenariats_texte') }}
       </p>
     </div>
   </div>
